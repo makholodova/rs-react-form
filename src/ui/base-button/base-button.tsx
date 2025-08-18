31d2443083c187cbac16b066e69444c62eed7b13
Marina Kholodova <marinaholodova@gmail.com>
2025-08-18 23:03:39 +0300

    feat: add base-button

---
+++ src/ui/base-button/base-button.tsx
@@ -0,0 +1,21 @@
+﻿import { type FC } from 'react';
+import type { BaseButtonProps } from '../../types/ui.ts';
+import styles from './base-button.module.css';
+
+const BaseButton: FC<BaseButtonProps> = ({
+  type = 'button',
+  children,
+  className = '',
+  ...rest
+}) => {
+  return (
+    <button
+      type={type}
+      className={`${styles.baseButton} ${className}`}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
+};
+export default BaseButton;
