70cc021fde7e40f41a912045918d2f982660d985
Marina Kholodova <marinaholodova@gmail.com>
2025-08-20 23:27:01 +0300

    feat: display the newly entered data on the main page

---
+++ src/components/card/card.tsx
@@ -0,0 +1,35 @@
+﻿import type { User } from '../../types';
+import styles from './card.module.css';
+
+export default function Card({ user }: { user: User }) {
+  return (
+    <div className={styles.card}>
+      <img
+        className={styles.image}
+        alt={`${user.name}-photo`}
+        src={user.image}
+      />
+      <p className={styles.field}>
+        Name: <span>{user.name}</span>
+      </p>
+      <p className={styles.field}>
+        Age: <span>{user.age}</span>
+      </p>
+      <p className={styles.field}>
+        E-mail: <span>{user.email}</span>
+      </p>
+      <p className={styles.field}>
+        Password: <span>{user.password}</span>
+      </p>
+      <p className={styles.field}>
+        Password(confirm): <span>{user.confirmPassword}</span>
+      </p>
+      <p className={styles.field}>
+        Gender: <span>{user.gender}</span>
+      </p>
+      <p className={styles.field}>
+        Country: <span>{user.country}</span>
+      </p>
+    </div>
+  );
+}
--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,7 +1,24 @@
-﻿export default function Home() {
+﻿import { useSelector } from 'react-redux';
+import type { RootState } from '../../store/store.ts';
+import Card from '../card/card.tsx';
+import styles from './home.module.css';
+
+export default function Home() {
+  const uncontrolledForm = useSelector(
+    (state: RootState) => state.uncontrolledForm
+  );
+
   return (
     <div>
       <h1>Home Page</h1>
+      <div>
+        <h2>Uncuncontrolled Form</h2>
+        <div className={styles.cardList}>
+          {uncontrolledForm.users.map((user) => (
+            <Card key={user.email} user={user} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
