0c55d698a6646a4fc01945cfc08118f8a0808f4d
Marina Kholodova <marinaholodova@gmail.com>
2025-08-20 23:18:22 +0300

    feat: add countries-slice

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,11 @@
 ﻿import { configureStore } from '@reduxjs/toolkit';
 import modalReducer from './modal-slice.ts';
+import countriesReducer from './countries-slice.ts';
 
 const store = configureStore({
   reducer: {
     modal: modalReducer,
+    countries: countriesReducer,
   },
 });
 
---
+++ src/untils/getCountries.ts
@@ -0,0 +1,5 @@
+﻿import countries from '../data/countries.json';
+
+export default function getCountries() {
+  return countries.map((country) => country.name);
+}
