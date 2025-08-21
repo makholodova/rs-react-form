ecaa0e10567c2fb01b6b52d82de440f50f6bd559
Marina Kholodova <marinaholodova@gmail.com>
2025-08-21 17:57:51 +0300

    feat: add styles for not-found-page

--- src/components/not-found-page/not-found-page.tsx
+++ src/components/not-found-page/not-found-page.tsx
@@ -1,10 +1,13 @@
 import { Link } from 'react-router';
+import styles from './not-found-page.module.css';
 
 export default function NotFoundPage() {
   return (
-    <div>
-      <h1>404 Page Not Found</h1>
-      <Link to="/">Go Home</Link>
+    <div data-testid="not-found-page" className={styles.container}>
+      <h2 className={styles.title}>404 — Page Not Found</h2>
+      <Link className={styles.link} to="/">
+        Go Home
+      </Link>
     </div>
   );
 }
